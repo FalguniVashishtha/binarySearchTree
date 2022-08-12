d9cdfc9e89cc1a1666a30add4be51f3f10bfceaf
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-12 18:03:24 +0530

    Aility to search

--- src/com/bridgelabz/BinaryTree.java
+++ src/com/bridgelabz/BinaryTree.java
@@ -1,33 +1,28 @@
 package com.bridgelabz;
 
-class Node
-{
+class Node {
     int data;
     Node left;
     Node right;
+
 }
 public class BinaryTree
 {
-    //method to insert root and value
     public Node insert(Node root, int val)
     {
-
         if (root == null) {
-            return createNewNode(val);
+            return craeteNewNode(val);
         }
         if (val < root.data) {
             root.left = insert(root.left, val);
-
-        } else if (val > root.data)
-        {
+        } else if (val > root.data) {
             root.right = insert(root.right, val);
         }
         return root;
 
     }
-    //method name as createNewNode
-    public Node createNewNode(int k)
-    {
+    // method
+    public Node craeteNewNode(int k) {
         Node bst = new Node();
         bst.data = k;
         bst.left = null;
@@ -35,16 +30,40 @@ public class BinaryTree
         return bst;
     }
 
-    // create a method name as print , they are printing data
+    // method to print
     public void print(Node root) {
-
         if (root == null) {
             return;
         }
         print(root.left);
-        System.out.println(root.data);
 
-        //null right side
+        System.out.println(root.data);
         print(root.right);
+    }
+
+    public int size(Node node) {
+
+        if (node == null)
+            return 0;
+        else
+            return(size(node.left) + 1 + size(node.right));
+    }
+    public boolean nodePresent(Node root, int val)
+    {
+        if (root == null) {
+            return false;
+        }
+        Boolean isPresent = false;
+        while (root != null) {
+            if (val < root.data) {
+                root = root.left;
+            } else if (val > root.data) {
+                root = root.right;
+            } else {
+                isPresent = true;
+                break;
+            }
+        }
+        return isPresent;
     }
 }
--- src/com/bridgelabz/MainBinarySearchTree.java
+++ src/com/bridgelabz/MainBinarySearchTree.java
@@ -1,21 +1,38 @@
 package com.bridgelabz;
-
-public class MainBinarySearchTree {
+public class MainBinarySearchTree
+{
     public static void main(String[] args) {
-        System.out.println("welcome to Binary Search Tree" +
-                " program");
+        System.out.println("welcome to Binary Search Tree" + " program");
 
         BinaryTree bst = new BinaryTree();
         Node root = null;
-        // inserting the value in root
+
         root = bst.insert(root, 56); // 56 is value is adding in root
 
         root = bst.insert(root, 30);
 
         // 30->56->70->80
         root = bst.insert(root, 70);
+        root = bst.insert(root, 22);
+        root = bst.insert(root, 40);
+        root = bst.insert(root, 60);
+        root = bst.insert(root, 95);
+        root = bst.insert(root, 11);
+        root = bst.insert(root, 65);
+        root = bst.insert(root, 3);
+        root = bst.insert(root, 6);
+        root = bst.insert(root, 63);
+        root = bst.insert(root, 67);
 
         bst.print(root);
 
+        System.out.println(bst.nodePresent(root, 63));
+
+        if(root != null)
+            System.out.println("\nElement 63 is present in the binary tree");
+        else
+            System.out.println("\nElement 63 is not present in the binary tree");
+
     }
 }
+
